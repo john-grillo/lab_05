92fc2615bc7bf7ab11d8a9acf3f9e2e564979c97
twilight-wizard <john.grillo@pcc.edu>
2016-11-11 15:10:01 -0800

    passed question 3. Aaron gave me permission to change the test so it fetched the 3rd and 4th items in the array. And removed the newline character in my app.js so it passed. Thank you, Aaron.

--- lab/tests.js
+++ lab/tests.js
@@ -17,7 +17,8 @@ function testMultiply() { //eslint-disable-line
 }
 
 function testSumAndMultiply() { //eslint-disable-line
-  if (sumAndMultiply(4, 7, 5)[1] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[2] === 'The product of 4 and 7 and 5 is 140.') {
+  //lovingly changed with Aaron's permission to fetch spots 2 and 3 in the array so that I could pass the test. Thank you, Aaron.
+  if (sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
     console.log('%c TEST FOR sumAndMultiply() PASSES', 'color: green');
   } else {
     console.log('%c TEST FOR sumAndMultiply() FAILS', 'color: red');
